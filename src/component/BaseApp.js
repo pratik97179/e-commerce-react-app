f5396896160dc9c4870e70cfae8df6f5471ad01a
Shubham Kumar <rdfsshubham@gmail.com>
2023-10-12 01:28:50 +0530

    navbar basic sturcture

--- src/component/BaseApp.js
+++ src/component/BaseApp.js
@@ -1,9 +1,11 @@
 import Header from "./Header.js";
+import Navbar from "./Navbar.js";
 
 const BaseApp = () => {
     return (
         <div className="base-app-wrapper">
             <Header />
+            <Navbar/>
         </div>
     );
 };
