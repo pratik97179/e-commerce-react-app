584be9473e3bd23e6ee209cc28195c4eb446a9be
Pratik Nath Tiwari <pratik@techalchemy.co>
2023-10-11 23:01:05 +0530

    Base file added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import BaseApp from "./component/BaseApp";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <BaseApp />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/component/BaseApp.js
@@ -0,0 +1,7 @@
+import Header from "./Header";
+
+const BaseApp = () => {
+    return <Header />;
+};
+
+export default BaseApp;
